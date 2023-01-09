a8f50755a3b0b4b57a2058ac01d7ec8d90ce3e35
Dodokhon <abdulloh.ustb@gmail.com>
2023-01-09 09:12:51 +0300

    condition

---
+++ js/condition.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/* if (4 == 5) {
+    console.log('ok');
+} else {
+    console.log('not error');
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('error');
+} else if (num > 51) {
+    console.log('много');
+} else {
+    console.log('ok');
+} */
+
+const num2 = 65;
+
+switch (num2) {
+    case 60: 
+        console.log('Wrong');
+        break;
+    case 70:
+        console.log('Wrong too');
+        break;
+    case 55:
+        console.log('All right');
+        break;    
+    default:
+        console.log('Find right solution');
+        break;
+}
