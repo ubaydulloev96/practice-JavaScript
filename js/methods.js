0469df1573c278311579b63901bfa63b93a55e87
Dodokhon <abdulloh.ustb@gmail.com>
2023-01-11 15:56:55 +0300

    methods

---
+++ js/methods.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* const str = 'river';
+const arr = [1, 3, 41, 34];
+
+console.log(str.length);
+console.log(arr.length); */
+
+/* const str = 'mouNtain';
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase()); */
+
+/* const fruits = 'apple, banana, pomelo, orange, grape';
+
+console.log(fruits.indexOf('orange')); */
+
+//  const greeting = 'Hello world';
+
+//  console.log(greeting.slice(6));
+//  console.log(greeting.substring(4, 9));   // substring не поддерживает отрицательные значения
+//  console.log(greeting.substr(6, 3));     // второй аргумент означает сколько символов будет написан
+
+/* const num = 12.7;
+console.log(Math.round(num)); // округление в ближайшего число */
+
+const num = '12.2px';
+console.log(parseInt(num));
